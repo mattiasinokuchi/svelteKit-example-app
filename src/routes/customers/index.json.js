60bce7cbb22d088720b3d50a1d5b0666d203de3e
mattiasinokuchi <8-b@tuta.io>
2021-09-07 14:22:40 +0200

    added post function

--- src/routes/customers/index.json.js
+++ src/routes/customers/index.json.js
@@ -1,8 +1,32 @@
 import supabase from '$lib/db';
 
 export const get = async (_) => {
-    let { data } = await supabase.from('customers').select('*');
+    let { data } = await supabase
+        .from('customers')
+        .select('*');
     return {
         body: data
+    };
+};
+
+export const post = async (request) => {
+    console.log(request);
+    const { data, error } = await supabase
+        .from('customers')
+        .upsert({
+            lastName: request.body.get('text'),
+        });
+
+    if (!error && request.headers.accept !== 'application/json') {
+        return {
+            status: 303,
+            headers: {
+                location: '/customers'
+            }
+        };
+    }
+
+    return {
+        body: data[0]
     };
 };
