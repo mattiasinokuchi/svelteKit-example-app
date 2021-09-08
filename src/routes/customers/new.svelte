b8d20be34ef45b30909eb3291219e5e796d282dc
mattiasinokuchi <8-b@tuta.io>
2021-09-08 11:28:59 +0200

    added styling

--- src/routes/customers/new.svelte
+++ src/routes/customers/new.svelte
@@ -7,27 +7,98 @@
 	});
 </script>
 
-<form class="new" action="/customers.json" method="post">
-	<label for="firstName">First Name</label>
-	<input
-		name="firstName"
-		aria-label="Add customer"
-		placeholder="First name"
-	/>
-	<br>
-	<label for="lastName">Last Name</label>
-	<input
-		name="lastName"
-		aria-label="Add customer"
-		placeholder="Last name" />
-	<br />
-	{#each products as { name }}
-		<input
-			name="product"
-			type="checkbox"
-		/>
-		<label for="product">{name}</label>
-		<br />
-	{/each}
-	<input type="submit" value="submit" />
+<form action="/customers.json" method="post">
+	<ul>
+		<li>
+			<label for="firstName">First Name</label>
+			<input
+				type="text"
+				id="firstName"
+				name="firstName"
+				aria-label="Add customer"
+				placeholder="First name"
+			/>
+		</li>
+		<li>
+			<label for="lastName" type="text">Last Name</label>
+			<input
+				type="text"
+				id="lastName"
+				name="lastName"
+				aria-label="Add customer"
+				placeholder="Last name"
+			/>
+		</li>
+		<li>
+			{#each products as { name }}
+				<div>
+					<input name="product" type="checkbox" />
+					<label for="product">{name}</label>
+				</div>
+			{/each}
+		</li>
+		<li class="button">
+			<button type="submit">Submit</button>
+		</li>
+	</ul>
 </form>
+
+<style>
+	form {
+		/* Center the form on the page */
+		margin: 0 auto;
+		width: 40vw;
+		/* Form outline */
+		padding: 1em;
+		border: 1px solid #ccc;
+		border-radius: 1em;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	form li + li {
+		margin-top: 1em;
+	}
+
+	label {
+		/* Uniform size & alignment */
+		width: 20vw;
+		text-align: right;
+	}
+
+	input {
+		/* To make sure that all text fields have the same font settings
+     By default, textareas have a monospace font */
+		font: 1em sans-serif;
+
+		/* Match form field borders */
+		border: 1px solid #999;
+	}
+
+	#firstName,
+	#lastName {
+		/* Uniform text field size */
+		width: 300px;
+		box-sizing: border-box;
+	}
+
+	input:focus {
+		/* Additional highlight for focused elements */
+		border-color: #000;
+	}
+
+	.button {
+		/* Align buttons with the text fields */
+		padding-left: 90px; /* same size as the label elements */
+	}
+
+	button {
+		/* This extra margin represent roughly the same space as the space
+     between the labels and their text fields */
+		margin-left: 0.5em;
+	}
+</style>
