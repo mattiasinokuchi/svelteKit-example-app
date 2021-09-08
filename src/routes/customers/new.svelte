369230c4b4d54ad3674ddb93b8574f52b466d596
mattiasinokuchi <8-b@tuta.io>
2021-09-08 10:16:46 +0200

    added list of products

--- src/routes/customers/new.svelte
+++ src/routes/customers/new.svelte
@@ -1,23 +1,33 @@
 <script>
+	import { onMount } from "svelte";
+	let products = [];
+	onMount(async () => {
+		const res = await fetch(`/subscriptions.json`);
+		products = await res.json();
+	});
 </script>
 
-<form
-	class="new"
-	action="/customers.json"
-	method="post"
->
+<form class="new" action="/customers.json" method="post">
 	<input
 		name="firstName"
 		aria-label="Add customer"
 		placeholder="First name"
 	/>
-	<input
-		name="lastName"
-		aria-label="Add customer"
-		placeholder="Last name"
-	/>
-	<input type="submit" value="submit">
+	<input name="lastName" aria-label="Add customer" placeholder="Last name" />
+	<input type="submit" value="submit" />
 </form>
+
+{#await products}
+	<p>...waiting</p>
+{:then products}
+	<p>Available products are: 
+	{#each products as {name}}
+		<p>{name}</p>
+	{/each}
+</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
 
 <style>
 	.new input {
