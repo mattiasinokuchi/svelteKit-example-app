f3becb37fa5f02bd8c94ea1d8a318ce8fefcc900
mattiasinokuchi <8-b@tuta.io>
2021-09-09 14:12:11 +0200

    styling

--- src/routes/subscriptions/index.svelte
+++ src/routes/subscriptions/index.svelte
@@ -7,10 +7,10 @@
 				props: { subscription },
 			};
 		}
-        const { message } = await res.json();
-        return {
-            error: new Error(message),
-        };
+		const { message } = await res.json();
+		return {
+			error: new Error(message),
+		};
 	}
 </script>
 
@@ -19,26 +19,40 @@
 </script>
 
 <main>
-	{#each subscription as { name, emoji, id }}
-		<a sveltekit:prefetch href={`/subscriptions/${id}`} class="box">
-			<h2>{name} {emoji}</h2>
-		</a>
-	{/each}
+	<ul>
+		{#each subscription as { name, emoji, id }}
+			<li class="box">
+				<a sveltekit:prefetch href={`/subscriptions/${id}`}>
+					<h2>{name}</h2>
+					<h2>{emoji}</h2>
+				</a>
+			</li>
+		{/each}
+	</ul>
 </main>
 
 <style>
 	main {
 		display: flex;
-		flex-wrap: wrap;
 		justify-content: center;
 	}
 	.box {
 		padding: 0.25rem;
 		margin: 1.5rem;
-		color: salmon;
 		box-shadow: 4px 5px 11px 2px lightgray;
+		width: 40vw;
 	}
 	.box:hover {
 		box-shadow: 4px 5px 11px 10px lightgray;
+	}
+	ul {
+		list-style-type: none;
+	}
+	h2 {
+		color: salmon;
+	}
+	a {
+		text-decoration: none;
+		color: salmon;
 	}
 </style>
