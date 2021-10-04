bda9cf7cff12f95ab3063141538d84b20f3b9173
mattiasinokuchi <8-b@tuta.io>
2021-10-04 14:40:05 +0200

    removed home page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,9 +8,8 @@
 </svelte:head>
 
 <nav>
-	<a class:active={$page.path === '/'} sveltekit:prefetch href="/">HOME</a>
-	<a class:active={$page.path === '/customer'} sveltekit:prefetch href="/customer">CUSTOMERS</a>
 	<a class:active={$page.path === '/product'} sveltekit:prefetch href="/product">PRODUCTS</a>
+	<a class:active={$page.path === '/customer'} sveltekit:prefetch href="/customer">CUSTOMERS</a>
 	<a class:active={$page.path === '/schedule'} sveltekit:prefetch href="/schedule">SCHEDULE</a>
 	<a class:active={$page.path === '/order'} sveltekit:prefetch href="/order">ORDERS</a>
 	<a class:active={$page.path === '/delivery'} sveltekit:prefetch href="/delivery">DELIVERIES</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,9 @@
-{#if false}<slot></slot>{/if}
-<main>
-	<h1 class="box"><a sveltekit:prefetch href="/customer" >CUSTOMERS</a></h1>
-	<h1 class="box"><a sveltekit:prefetch href="/product" >PRODUCTS</a></h1>
-	<h1 class="box"><a sveltekit:prefetch href="/schedule" >SCHEDULE</a></h1>
-	<h1 class="box"><a sveltekit:prefetch href="/order" >DELIVERY</a></h1>
-	<h1 class="box"><a sveltekit:prefetch href="/delivery" >DELIVERIES</a></h1>
-</main>
 
-<style>
-	main {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-	}
-	h1 {
-		width: 60vw;
-	}
-	.box {
-		padding: 0.25rem;
-		margin: 1.5rem;
-		color: salmon;
-		box-shadow: 4px 5px 11px 2px lightgray;
-	}
-	.box:hover {
-		box-shadow: 4px 5px 11px 10px lightgray;
-	}
-	a {
-		text-decoration: none;
-		color: salmon;
-	}
-</style>
+<script context="module">
+    export async function load() {
+        return {
+            status: 302,
+            redirect: "/product"
+        };
+    }
+</script>
