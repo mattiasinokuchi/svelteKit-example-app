6351906b0962925e8020bb35e9d99c217441ad64
mattiasinokuchi <8-b@tuta.io>
2021-10-08 12:50:04 +0200

    undelivered products will not appear in list

--- src/routes/billing/index.svelte
+++ src/routes/billing/index.svelte
@@ -21,52 +21,48 @@
 
 	for (let i = 0; i < customer.length; i++) {
 		const sum = customer[i].order_.reduce(function (acc, cur) {
-			if(cur.past_delivery != null) {
+			if (cur.past_delivery != null) {
 				return acc + cur.past_delivery.length * cur.product.price;
 			}
 		}, 0);
-		customer[i]['toPay'] = sum;
+		customer[i]["toPay"] = sum;
 	}
 </script>
 
 <main>
-	<h2 hidden={customer.length>0}>
-		No billing to do. Relax!
-	</h2>
+	<h2 hidden={customer.length > 0}>No billing to do. Relax!</h2>
 	<ul>
 		{#each customer as { first_name, last_name, order_, toPay }}
-		{#if toPay != null && toPay !== 0}
-		<li class="box">
-			<h2>
-				{first_name}
-				{last_name}:
-			</h2>
-			{#each order_ as { id, past_delivery, product }}
-				{#if past_delivery != null}
-					{product.name}, ${product.price}:
-					<form action="/billing.json" method="post">
-						<ul>
-							<li>
-								<input hidden name="id" value={id} />
-								{#if past_delivery != null}
-									{#each past_delivery as date}
-										<ul>
-											<li>
-												{date}
-											</li>
-										</ul>
-									{/each}
-								{/if}
-							</li>
-						</ul>
-						<input type="submit" value="Clear" />
-					</form>
-					<br />
-				{/if}
-			{/each}
-			Total: ${toPay}
-		</li>
-		{/if}
+			{#if toPay != null && toPay !== 0}
+				<li class="box">
+					<h2>
+						{first_name}
+						{last_name}:
+					</h2>
+					{#each order_ as { id, past_delivery, product }}
+						{#if past_delivery != null && past_delivery.length > 0}
+							{product.name}, ${product.price}:
+							<form action="/billing.json" method="post">
+								<ul>
+									<li>
+										<input hidden name="id" value={id} />
+										{#each past_delivery as date}
+											<ul>
+												<li>
+													{date}
+												</li>
+											</ul>
+										{/each}
+									</li>
+								</ul>
+								<input type="submit" value="Clear" />
+							</form>
+							<br />
+						{/if}
+					{/each}
+					Total: ${toPay}
+				</li>
+			{/if}
 		{/each}
 	</ul>
 </main>
