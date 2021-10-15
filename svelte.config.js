9d8951e207dc76b9f1825e367c26720cac5182e1
mattiasinokuchi <8-b@tuta.io>
2021-10-15 12:39:03 +0200

    added ENV in config

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,12 @@
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			define: {
+				'process.env': process.env
+			}
+		}
+
 	}
 };
-
-export default config;
