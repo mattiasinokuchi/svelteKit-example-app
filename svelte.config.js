9376e178f2359e0fe7d31e5dff64ada16fe7b4f6
mattiasinokuchi <8-b@tuta.io>
2021-10-15 12:26:22 +0200

    try fix error with deployment on Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,12 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter(),
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			define: {
+				'process.env': process.env
+			}
+		}
 	}
 };
 
