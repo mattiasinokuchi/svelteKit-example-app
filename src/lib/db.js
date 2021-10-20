631a537ca7a8b72cff2bdda5004ec3786d18a64d
mattiasinokuchi <8-b@tuta.io>
2021-10-20 15:03:03 +0200

    added connection using node-postgres library

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,4 +5,19 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
 
-export default supabase
+export default supabase;
+
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'postgres',
+  host: import.meta.env.VITE_SUPABASE_HOST,
+  database: 'postgres',
+  password: import.meta.env.VITE_SUPABASE_PASSWORD,
+  port: import.meta.env.VITE_SUPABASE_PORT,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
