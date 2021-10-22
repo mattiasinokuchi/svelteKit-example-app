fd7054378043427452f9905e28f3be8410fdb834
mattiasinokuchi <8-b@tuta.io>
2021-10-22 09:38:46 +0200

    changed queries to SQL

--- src/routes/product/index.json.js
+++ src/routes/product/index.json.js
@@ -1,17 +1,19 @@
 /*  This module contains endpoints to the database
     for the product page   */
 
-import supabase, { pool } from '$lib/db';
-
-// Hello database
-(async () => {
-    const res = await pool.query('SELECT $1::text as message', ['Hello world!'])
-    console.log(res.rows[0].message) // Hello world!
-    await pool.end()
-})();
+import { pool } from '$lib/db';
+//import supabase from '$lib/db';
 
 // Reads all products
 export const get = async (_) => {
+    const res = await pool.query('SELECT * FROM product');
+    return {
+        body: res.rows
+    };
+};
+
+// Reads all products
+/*export const get = async (_) => {
     let { data } = await supabase
         .from('product')
         .select(`
@@ -22,10 +24,33 @@ export const get = async (_) => {
     return {
         body: data
     };
-};
+};*/
 
 // Add a new product
 export const post = async (request) => {
+    const values = [
+        request.body.get('name'),
+        request.body.get('price'),
+        request.body.get('emoji')
+    ];
+    try {
+        await pool.query(
+            'INSERT INTO product(name, price, emoji) VALUES($1, $2, $3) RETURNING *',
+            values
+        );
+        return {
+            status: 303,
+            headers: {
+                location: '/product'
+            }
+        };
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+// Add a new product
+/*export const post = async (request) => {
     const { data, error } = await supabase
         .from('product')
         .upsert({
@@ -44,4 +69,4 @@ export const post = async (request) => {
     return {
         body: data
     };
-};
+};*/
