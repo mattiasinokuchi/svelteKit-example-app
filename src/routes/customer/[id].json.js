4e075d17a93681180debad0947c0305b29bc96c8
mattiasinokuchi <8-b@tuta.io>
2021-10-25 15:43:08 +0200

    changed queries to SQL

--- src/routes/customer/[id].json.js
+++ src/routes/customer/[id].json.js
@@ -1,18 +1,16 @@
 /*  This module contains endpoints to the database
     for the customers parent and child pages   */
 
-import supabase from '$lib/db';
 import { pool } from '$lib/db';
-
 
 //  Reads data for a specific customer
 export const get = async ({ params }) => {
     try {
         const { id } = params;
-        const res = await pool.query(
-            `SELECT * FROM customer
-            WHERE id = $1`,
-            [id]
+        const res = await pool.query(`
+            SELECT * FROM customer
+            WHERE id = $1
+            `, [id]
         );
         return {
             body: res.rows[0]
@@ -22,24 +20,27 @@ export const get = async ({ params }) => {
     }
 }
 
-/* Deletes a customer */
+//  Deletes a customer
 export const del = async (request) => {
     /*if (!request.locals.user) {
         return { status: 401 };
     }*/
-    const { error } = await supabase
-        .from('customer')
-        .delete().match({
-            id: request.params.id,
-            //user_id: request.locals.user.id
-        });
-
-    if (!error && request.headers.accept !== 'application/json') {
+    const id = request.params.id;
+    try {
+        /*  Avoids string concatenating parameters into the
+            query text directly to prevent sql injection    */
+        await pool.query(`
+            DELETE FROM customer
+            WHERE id = $1`,
+            [id]
+        );
         return {
             status: 303,
             headers: {
                 location: '/customer'
             }
         };
+    } catch (error) {
+        console.log(error)
     }
 };
