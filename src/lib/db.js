a1a49b5fa073a41b9aaad726f35452e977eae302
mattiasinokuchi <8-b@tuta.io>
2021-11-02 12:40:30 +0100

    removed Supabase

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-export default supabase;
-
 import { Pool } from 'pg';
 
 export const pool = new Pool({
