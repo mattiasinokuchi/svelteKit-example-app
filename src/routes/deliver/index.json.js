1a253a55c7bc26b251ec10709f68feb62346ad81
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-11-16 06:40:36 +0100

    replaced loop for query

--- src/routes/deliver/index.json.js
+++ src/routes/deliver/index.json.js
@@ -5,76 +5,68 @@ import { pool } from '$lib/db';
 
 export const get = async (_) => {
     try {
-        // Declare an array to store responses from looped query
-        let deliveries = [];
-        // Loop query until all deliveries within 90 days are returned
-        for (let index = 0; index < 90; index++) {
-            const res = await pool.query(`
-                SELECT
-                    -- delivery date (converted to yyyy-mm-dd) given by...
-                    TO_CHAR(
-                        --- ...todays date...
-                        CURRENT_DATE +
-                        -- ...added to a serie using the delivery interval...
-                        $1*delivery_interval -
-                        -- ...substracted by the remainder (days since last delivery)...
-                        MOD(
-                            -- ...after division of days since delivery started...
-                            CURRENT_DATE - start_date,
-                            -- ...by the delivery interval (remainder is 0 on delivery days)...
-                            delivery_interval),
+        const res = await pool.query(`
+            SELECT
+                TO_CHAR(
+                    --- Todays date...
+                    CURRENT_DATE +
+                    -- ...added to a serie using the delivery interval...
+                    index*delivery_interval -
+                    -- ...substracted by the remainder (days since last delivery)...
+                    MOD(
+                        -- ...after division of days since delivery started...
+                        CURRENT_DATE - start_date,
+                        -- ...by the delivery interval (remainder is 0 on delivery days)...
+                        delivery_interval),
+                        -- ...converted to yyyy-mm-dd
                         'yyyy-mm-dd')
-                    AS delivery_date,
-                    product_name,
-                    COUNT (product_name)
-                FROM
-                    order_table
-                INNER JOIN
-                    product_table
-                ON
-                    product_table.id = order_table.product_id
-                INNER JOIN
-                    customer_table
-                ON
-                    customer_table.id = order_table.customer_id
-                WHERE
-                    -- subscription is activated
-                    customer_table.active = 'true'
-                AND
-                    -- no time-out on selected day
-                    customer_id
+                AS delivery_date,
+                product_name,
+                COUNT (product_name)
+            FROM
+                order_table
+            INNER JOIN
+                product_table
+            ON
+                product_table.id = order_table.product_id
+            INNER JOIN
+                customer_table
+            ON
+                customer_table.id = order_table.customer_id
+            CROSS JOIN
+                generate_series(0,89) AS index
+            WHERE
+                -- subscription is activated
+                customer_table.active = 'true'
+            AND
+                -- no time-out on selected day
+                customer_id
                     NOT IN (
                         SELECT customer_id
                         FROM time_out_table
-                        WHERE ((CURRENT_DATE + $1*delivery_interval) BETWEEN start_time::date AND end_time)
+                        WHERE ((CURRENT_DATE + index*delivery_interval) BETWEEN start_time::date AND end_time)
                     )
-                AND     
-                    -- not delivered
-                    order_table.id
+            AND     
+                -- not delivered
+                order_table.id
                     NOT IN (
                         SELECT order_id
                         FROM delivery_table
-                        WHERE delivery_time::date = (CURRENT_DATE + $1*delivery_interval) 
+                        WHERE delivery_time::date = (CURRENT_DATE + index*delivery_interval) 
                     )
-                AND
-                    -- delivery within 90 days (to prevent products with shorter
-                    -- delivery interval being replaced by products with longer interval)
-                    $1*delivery_interval < 90
-                GROUP BY
-                    product_name,
-                    -- delivery date (same expression as above)
-                    CURRENT_DATE +
-                    $1*delivery_interval -
-                    MOD((CURRENT_DATE - start_date), delivery_interval)
-                ORDER BY
-                    product_name;
-            `, [index]);
-            // Add response to array
-            res.rows.forEach(element => {
-                deliveries.push(element);
-            });
-        }
-        const deliveriesSortedByDate = deliveries.sort((a, b) => {
+            AND
+                -- delivery within 90 days (to prevent products with shorter delivery interval being replaced by products with longer interval)
+                index*delivery_interval < 90
+            GROUP BY
+                product_name,
+                -- delivery date (same expression as above)
+                CURRENT_DATE +
+                index*delivery_interval -
+                MOD((CURRENT_DATE - start_date), delivery_interval)
+            ORDER BY
+                product_name;
+            `);
+        const deliveriesSortedByDate = res.rows.sort((a, b) => {
             return new Date(a.delivery_date) - new Date(b.delivery_date)
         });
         //  Group deliveries by date
