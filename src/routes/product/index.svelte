970e804a64b0dfa26e0687cdcd2aacb69aebdd02
mattiasinokuchi <8-b@tuta.io>
2021-11-22 09:22:34 +0100

    form is shown when header is clicked

--- src/routes/product/index.svelte
+++ src/routes/product/index.svelte
@@ -18,46 +18,59 @@
 <script>
 	export let product;
 	let subscription = false;
+	let formHidden = true;
 </script>
 
 <main>
 	<!-- This is a form for adding new products -->
-	<form class="box" action="/product.json" method="post">
-		<h2>New product</h2>
-		<input
-			type="checkbox"
-			id="subscription"
-			name="subscription"
-			bind:checked={subscription}
-		/>
-		<label for="subscription">Subscription</label>
-		<br />
-		<label hidden={!subscription} for="delivery_interval"
-			>Delivery interval (days)</label
-		>
-		<input
-			hidden={!subscription}
-			type="number"
-			min="1"
-			max="999"
-			value="7"
-			id="delivery_interval"
-			name="delivery_interval"
-			aria-label="Add product"
-		/>
-		<br />
-		<label for="product_name">Name</label>
-		<input
-			type="text"
-			id="product_name"
-			name="product_name"
-			aria-label="Add product"
-		/>
-		<br />
-		<label for="price">Price ($)</label>
-		<input type="number" id="price" name="price" aria-label="Add product" />
-		<br />
-		<button type="submit">Submit</button>
+	<form
+		class="box"
+		action="/product.json"
+		method="post"
+		on:click={() => (formHidden = false)}
+	>
+		<h2>Add new product</h2>
+		<div hidden={formHidden}>
+			<input
+				type="checkbox"
+				id="subscription"
+				name="subscription"
+				bind:checked={subscription}
+			/>
+			<label for="subscription">Subscription</label>
+			<br />
+			<label hidden={!subscription} for="delivery_interval"
+				>Delivery interval (days)</label
+			>
+			<input
+				hidden={!subscription}
+				type="number"
+				min="1"
+				max="999"
+				value="7"
+				id="delivery_interval"
+				name="delivery_interval"
+				aria-label="Add product"
+			/>
+			<br />
+			<label for="product_name">Name</label>
+			<input
+				type="text"
+				id="product_name"
+				name="product_name"
+				aria-label="Add product"
+			/>
+			<br />
+			<label for="price">Price ($)</label>
+			<input
+				type="number"
+				id="price"
+				name="price"
+				aria-label="Add product"
+			/>
+			<br />
+			<button type="submit">Submit</button>
+		</div>
 	</form>
 
 	<h2 hidden={product.length > 0}>No products. Add something!</h2>
