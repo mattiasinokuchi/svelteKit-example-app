7f49a257d96bfd8ffe2332165484275594b68f43
mattiasinokuchi <8-b@tuta.io>
2021-11-23 09:05:38 +0100

    test to fix deployed app

--- src/routes/customer/index.svelte
+++ src/routes/customer/index.svelte
@@ -5,12 +5,12 @@
 		try {
 			res = await fetch("/customer.json");
 			const customer = await res.json();
-//			res = await fetch("/customer/get_phone_numbers.json");
-//			const phone = await res.json();
+			res = await fetch("/customer/get_phone_numbers.json");
+			const phone = await res.json();
 			return {
 				props: {
 					customer,
-//					phone,
+					phone,
 				},
 			};
 		} catch (error) {
