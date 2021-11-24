c17c46cbdaf7c62fe24018f87d2e2af1e1742e69
mattiasinokuchi <8-b@tuta.io>
2021-11-24 09:22:59 +0100

    old time-out's removed at request

--- src/routes/customer/get_time_out/[customer_id].json.js
+++ src/routes/customer/get_time_out/[customer_id].json.js
@@ -5,7 +5,13 @@ import { pool } from '$lib/db';
 
 export const get = async ({ params }) => {
     try {
+        // Remove old time-out's
+        await pool.query(`
+            DELETE FROM time_out_table
+            WHERE end_time :: DATE < CURRENT_DATE;
+        `);
         const { customer_id } = params;
+        //  Get time-out's
         const res = await pool.query(`
             SELECT
                 id AS time_out_id,
