69d6aaff8cd8a459501ae5e2b0045e1e3ceefda8
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-11-30 10:00:04 +0100

    redirect to HTTPS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,7 @@
     export async function load() {
         return {
             status: 302,
-            redirect: "/product",
-            headers: {
-                "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload"
-            }
+            redirect: "/product"
         };
     }
 </script>
