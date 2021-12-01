e2580b68681986c57610235d0e02381230d79782
mattiasinokuchi <8-b@tuta.io>
2021-12-01 14:47:04 +0100

    test for redirect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 
 <script context="module">
-    export async function load() {
+    export async function load(request) {
+        console.log(request);
         return {
             status: 302,
             redirect: "/product"
