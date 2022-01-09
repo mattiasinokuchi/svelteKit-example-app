a55e3b7a765f65f3c03a87d706da061c80603b6e
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2022-01-09 16:48:04 +0100

    send magic link

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,16 +5,27 @@ const url = `https://${process.env.AUTH0_DOMAIN}/passwordless/start`;
 export async function post(request) {
     console.log(request.body.get('email'));
     const res = await fetch(url, {
-        method: post,
+        method: 'POST',
         headers: {
-            
-        }
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            client_id: process.env.AUTH0_CLIENT_ID,
+            client_secret: process.env.AUTH0_CLIENT_SECRET,
+            connection: 'email',
+            email: request.body.get('email'),
+            send: 'link',
+            authParams: {
+                scope: 'openid',
+                state: 'YOUR_STATE'
+            }
+        })
     });
+    console.log(res.body);
     return {
-
-
-        body: {
-            message: 'request recieved from ' + request.body.get('email')
-        }
+        status: 303,
+        location: request.headers.referer,
+        body: 'Please log in using the magic link sent to ' + request.body.get('email') + '!'
     }
 }
